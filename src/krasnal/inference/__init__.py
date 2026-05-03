3907799582915024bc67fada25e7d535b9e1476a
Igor Jakus <igorjakus@protonmail.com>
2026-05-03 12:10:57 +0200

    Deduplicate legal_token_ids(board)

--- src/krasnal/inference/__init__.py
+++ src/krasnal/inference/__init__.py
@@ -3,14 +3,13 @@ from krasnal.inference.exceptions import NoLegalMovesError
 from krasnal.inference.game import Game
 from krasnal.inference.sampling import sample_token
 from krasnal.inference.session import InferenceSession
-from krasnal.inference.utils import get_legal_token_ids, load_model
+from krasnal.inference.utils import load_model
 
 __all__ = [
     "Game",
     "InferenceSession",
     "NoLegalMovesError",
     "StatelessBatchInferenceSession",
-    "get_legal_token_ids",
     "load_model",
     "sample_token",
 ]
