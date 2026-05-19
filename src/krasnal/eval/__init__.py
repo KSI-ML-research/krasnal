a98dc9fe26dc88e51e3447f80d4c750b4260e4b9
Igor Jakus <igorjakus@protonmail.com>
2026-05-19 20:42:30 +0200

    refactor: Remove redundant eval config

--- src/krasnal/eval/__init__.py
+++ src/krasnal/eval/__init__.py
@@ -1,8 +1,6 @@
-from .config import EvalConfig
 from .evaluator import ChessEvaluator, chess_evaluator_from_config
 
 __all__ = [
     "ChessEvaluator",
-    "EvalConfig",
     "chess_evaluator_from_config",
 ]
